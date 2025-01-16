3a71881a9ced356a58adffa891bf34b5294ab934
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2025-01-16 14:06:05 +0300

    importing usercard component inside a app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Components/Header.jsx';
 import Counter from './Cmponents/Counter.jsx';
+import UserCard from './Components/Usercard.jsx';
 
 const App = () => {
   return (
